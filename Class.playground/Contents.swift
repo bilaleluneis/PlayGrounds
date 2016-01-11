363fe7b00eb55a1faa8ef39079c3ee805d5340ce
bilaleluneis <bilaleluneis@gmail.com>
2016-01-11 11:49:40 -0500

    more class snippets

--- Class.playground/Contents.swift
+++ Class.playground/Contents.swift
@@ -7,32 +7,49 @@ Playground code snippets to learn about Classes in Swift 2
 
 //classes can have dinit method that gets called when object is destroyed / removed from memory
 class Person {
-    var name:String
+    var firstName:String
+    var lastName:String
     var age:Int
     var weight:Float
     
+    //computed property
+    var fullName:String {
+        get{ return firstName + " " + lastName}
+        set(name){
+            self.fullName =  name
+        }
+    }
+    
     init(){
-        self.name = ""
+        self.firstName = ""
+        self.lastName = ""
         self.age  = 0
         self.weight = 0.0
     }
     
-    init(name:String, age:Int, weight:Float){
-        self.name = name
+    init(firstName:String, lastName:String, age:Int, weight:Float){
+        self.firstName = firstName
+        self.lastName = lastName
         self.age = age
         self.weight = weight
     }
     
     deinit{
-        print("Person instance with name: \(name) is now destroyed")
+        print("Person instance with name: \(firstName) is now destroyed")
     }
 }
 
 //person instance will be destroyed once it reaches the end scope of the function
-func testPersonDinit(personName:String, personAge:Int, personWeight:Float){
-    Person(name:personName, age:personAge, weight:personWeight)
+func testPersonDinit(personFirstName:String, personLastName:String, personAge:Int, personWeight:Float){
+    Person(firstName:personFirstName, lastName: personLastName, age:personAge, weight:personWeight)
 }
 
-testPersonDinit("Joe", personAge:20, personWeight:150.5)
+testPersonDinit("Joe",personLastName: "Doe", personAge:20, personWeight:150.5)
+
+var person1 = Person()
+person1.firstName = "James"
+person1.lastName = "Bond"
+let name = person1.fullName
+print(name)
 
 
