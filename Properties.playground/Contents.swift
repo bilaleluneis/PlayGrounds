dcb73d14b0777d98fa8edc2264825cf2ab55a572
bilaleluneis <bilaleluneis@gmail.com>
2016-01-15 18:41:55 -0500

    added computed properties snippet and properties with getter and setter

--- Properties.playground/Contents.swift
+++ Properties.playground/Contents.swift
@@ -4,3 +4,36 @@ Since: 01/11/2016
 ...
 Playground code snippets to learn about Properties in Swift 2
 */
+
+import Foundation
+
+struct Person {
+    var firstName = ""
+    var lastName = ""
+    
+    //computed property
+    var fullName:String { return firstName + " " + lastName }
+}
+
+var person1 = Person(firstName:"Joe",lastName: "Doe")
+
+struct Item {
+    var name = ""
+    var brand = ""
+    
+    //property with getter and setter
+    var ItemDescription:String {
+        get{ return name + " " + brand}
+        set{
+            //by default you have a property called newValue that holds the value you set to
+            var splittedName = newValue.componentsSeparatedByString(" ")
+            name = splittedName[0]
+            brand = splittedName[1]
+        }
+    }
+}
+
+var GroceryItem = Item(name: "fish", brand: "chickenSea")
+GroceryItem.ItemDescription
+GroceryItem.ItemDescription = "Tuna Fish"
+GroceryItem.ItemDescription
