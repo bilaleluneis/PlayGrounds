d8aa477e7e6bbbfd3b739f828b2d7f1ae3ee920b
bilaleluneis <bilaleluneis@gmail.com>
2016-01-17 08:49:10 -0500

    update code for property observers to show the use of oldValue

--- Properties.playground/Contents.swift
+++ Properties.playground/Contents.swift
@@ -37,3 +37,26 @@ var GroceryItem = Item(name: "fish", brand: "chickenSea")
 GroceryItem.ItemDescription
 GroceryItem.ItemDescription = "Tuna Fish"
 GroceryItem.ItemDescription
+
+struct Planet {
+    var count:Int = 0
+    //example of property observer that watches and act as property value change
+    var name:String {
+        willSet{ print("about to add planet\(newValue)") }
+        didSet{
+            print("old planet Name: \(oldValue)")
+            count++
+            print("number of planets now is \(count)")
+        }
+    }
+    
+    init(){
+       self.count = 0
+        self.name = ""
+    }
+    
+}
+
+var earth = Planet() //notice property observer didnt react on init()
+earth.name = "Earth"
+earth.name = "Earth2"
