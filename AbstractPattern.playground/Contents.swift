3208a20ab0939def944f67f1e73713006ebedf1b
Bilal El Uneis <bilaleluneis@yahoo.com>
2016-02-13 22:20:44 -0600

    implemented example of abstract pattern as currently supported in swift 2

---
+++ AbstractPattern.playground/Contents.swift
@@ -0,0 +1,12 @@
+/*:
+Author: Bilal El Uneis (bilaleluneis@icloud.com)
+Since: 02/13/2016
+...
+Playground code snippets to learn how to create Abstract classes in Swift 2
+*/
+
+//var abstract = Abstract() //this will throw runtime error
+
+var abstractInterface : Abstract = Concreet()
+
+abstractInterface.dump()
