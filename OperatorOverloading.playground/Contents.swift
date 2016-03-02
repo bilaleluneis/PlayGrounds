7218f204698a44432fddd9accbe3f98f8a3bb0d3
Bilal El Uneis <bilaleluneis@yahoo.com>
2016-03-02 15:30:01 -0500

    finished up  initial playground to try out operator overloading in swift 2

--- OperatorOverloading.playground/Contents.swift
+++ OperatorOverloading.playground/Contents.swift
@@ -17,24 +17,63 @@ enum Color {
 struct ColorCombination {
     var firstColor: Color
     var secondColor: Color
-    var resultColor: Color
+    var resultColor: Color?
     
-    init(firstColor:Color, secondColor:Color, resultColor:Color){
+    init(_ firstColor:Color, _ secondColor:Color, _ resultColor:Color) {
         self.firstColor = firstColor
         self.secondColor = secondColor
         self.resultColor = resultColor
     }
+    
+    init(_ firstColor:Color, _ secondColor:Color) {
+        self.firstColor = firstColor
+        self.secondColor = secondColor
+        self.resultColor = nil
+    }
 }
 
-//mark: the bellow methods still need implementation
+//list of color combination and resulting color
+let colorChart:[ColorCombination] = [ColorCombination(.red, .blue, .violet),
+                                     ColorCombination(.red, .yellow, .orange),
+                                     ColorCombination(.blue, .yellow, .green)]
+
+// overloads the == operator for ColorCombination struct type
 func == (colorCombo1:ColorCombination , colorCombo2:ColorCombination) -> Bool {
+    
+    if  colorCombo1.firstColor  == colorCombo2.firstColor &&
+        colorCombo1.secondColor == colorCombo2.secondColor {
+            return true
+    }
+    
+    if  colorCombo1.firstColor  == colorCombo2.secondColor &&
+        colorCombo1.secondColor == colorCombo2.firstColor {
+            return true
+    }
     return false
 }
 
-func + (firstColor:Color , secondColor:Color) -> Color {
+//overloads + for Color enum type
+func + (firstColor:Color , secondColor:Color) -> Color? {
     
     if firstColor == secondColor {
         return firstColor
     }
-    return .red
+    
+    let currentColorCombination = ColorCombination(firstColor, secondColor)
+    
+    for colorComboItemInList in colorChart {
+        //here we are using the == overloaded operator
+        if colorComboItemInList == currentColorCombination {
+            return colorComboItemInList.resultColor
+        }
+    }
+
+    return nil
 }
+
+//snippets to try out all that data structure above :)
+
+let resultColor1: Color? = .red + .blue
+let resultColor2 = .blue + .red //here im using type infrence
+let resultColor3 = .yellow + .blue
+let nilResultColor = .red + .orange
