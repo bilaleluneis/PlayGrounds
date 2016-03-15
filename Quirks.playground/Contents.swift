7cea9223f97ab679fa8df6801b4af3607e1bbc25
Bilal El Uneis <bilaleluneis@yahoo.com>
2016-03-14 22:11:16 -0400

    started playground to try out some quirks and things that swift allows us to do , but really doesn't make much sense to me why it is even allowed!

---
+++ Quirks.playground/Contents.swift
@@ -0,0 +1,35 @@
+/*:
+Author: Bilal El Uneis (bilaleluneis@icloud.com)
+Since: 03/14/2016
+...
+Playground code snippets to demo some weird but valid swift code
+*/
+
+/*
+    Structs are pass by Value Object, but a struct can actually
+    have a class definition inside it too ! wonder what that does
+    when struct i passed by value , but contain an object that is 
+    passed by refrence!
+*/
+
+public struct PushingStructs {
+    
+    // create class def inside struct
+    public class AclassInsideStruct {
+        public var a : String
+        internal var b : String
+        private var c : String
+        
+        init(){
+            a = "a"
+            b = "B"
+            c = "C"
+        }
+    }
+    
+    public var aClasssInstance: AclassInsideStruct
+    
+    public init(){
+        aClasssInstance = AclassInsideStruct()
+    }
+}
