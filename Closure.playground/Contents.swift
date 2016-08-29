70be1af5d422620d286206119ade4ea106a76e39
Bilal El Uneis <bilaleluneis@Bilals-MacBook-Air.local>
2016-08-29 01:50:37 -0400

    updated to comply with Swift 3 changes for sorting and collection method calls

--- Closure.playground/Contents.swift
+++ Closure.playground/Contents.swift
@@ -12,7 +12,7 @@ var add = {
 }
 
 //function that takes another function or closure
-func performOperation(n1:Int, _ n2:Int, _ operation: (Int,Int) -> Int?) -> Int? {
+func performOperation(_ n1:Int, _ n2:Int, _ operation: (Int,Int) -> Int?) -> Int? {
     return operation(n1,n2)
 }
 
@@ -26,20 +26,22 @@ var multiplyResult = performOperation(3, 5){
     (n1:Int,n2:Int) -> Int? in return n1 * n2
 }
 
+
 //using closures with Swift standard Libs
 let nonSortedArray:[Int] = [5,7,0,9,3,2]
 
-//applying sort logic using trailing closure
-let sortedArrayDec:[Int] = nonSortedArray.sort(){
-    (n1:Int,n2:Int) -> Bool in
-    return n2 < n1
-}
+//bellow is one way to call sorted and pass function / closure
+let sortedArrayDec:[Int] = nonSortedArray.sorted(by: {(n1:Int,n2:Int) -> Bool in return n2 < n1})
+
+//another valid syntax using trailing closure.. i actually think the above is more readable
+let sortedArrayUsingTrailingClosure = nonSortedArray.sorted(){ (n1:Int, n2:Int) -> Bool in return n2 < n1 }
+
 
 //Swift can infer arguments in closure so you dont need to specify them
-var sortedArrayAsc:[Int] = nonSortedArray.sort(){ return $1 > $0 }
+var sortedArrayAsc:[Int] = nonSortedArray.sorted(){ return $1 > $0 }
 print(sortedArrayAsc)
 
 //Swift can also infer the return statment when the closure or function passed is known to return
-sortedArrayAsc = nonSortedArray.sort(){$1 > $0}
+sortedArrayAsc = nonSortedArray.sorted(){$1 > $0}
 print(sortedArrayAsc)
 
